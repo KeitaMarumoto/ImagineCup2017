6d46b197a2d8ad4c77f228ac193c09d37b9a6543
Keita Marumoto <marumoto.keita@gmail.com>
2017-01-27 07:08:28 +0900

    ProductRegister.csを追加。
    ここでCSVなりJsonなりで持ってきたデータを登録する。

---
+++ ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Product/ProductRegister.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ProductRegister : MonoBehaviour {
+	[SerializeField]
+	List<string> name_;
+	[SerializeField]
+	List<float> unitPrice_;
+	[SerializeField]
+	List<float> cost_;
+	[SerializeField]
+	List<int> rank_;
+	[SerializeField]
+	List<int> secondsToSell_;
+
+	Dictionary<string, ProductData> products = new Dictionary<string, ProductData>();
+
+	// Use this for initialization
+	void Start ()
+	{
+		for (int productDataNum = 0; productDataNum < name_.Count; productDataNum++)
+		{
+			ProductData product_ = new ProductData(name_[productDataNum],
+												   unitPrice_[productDataNum],
+												   cost_[productDataNum],
+												   rank_[productDataNum],
+												   secondsToSell_[productDataNum]);
+			products.Add(product_.Name, product_);
+		}
+
+		foreach (KeyValuePair<string, ProductData> prod_ in products)
+		{
+			string message = "Name : " + prod_.Value.Name + " | " 
+				           + "UnitPrice : " + prod_.Value.UnitPrice.ToString() + " | " 
+				           + "Cost : " + prod_.Value.Cost.ToString() + " | "
+						   + "Rank" + prod_.Value.Rank.ToString() + " | "
+						   + "Seconds : " + prod_.Value.SecondsToSell.ToString() + " | "
+						   + "Number : " + prod_.Value.NumberOfProducts.ToString();
+			Debug.Log(message);
+		}
+	}
+}
