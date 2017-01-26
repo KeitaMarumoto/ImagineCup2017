1ab7491af7aade69841510a9c4a73612e18a8619
Keita Marumoto <marumoto.keita@gmail.com>
2017-01-27 06:25:17 +0900

    商品データのモデルクラスProductData.csを実装。
    コンストラクタに引数でデータを渡すことで商品の登録が可能。

---
+++ ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Product/ProductData.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 商品モデル
+/// </summary>
+public class ProductData {
+	/// <summary>
+	/// 名前
+	/// </summary>
+	public string Name { get; private set; }
+	/// <summary>
+	/// 単価
+	/// </summary>
+	public float UnitPrice { get; private set; }
+	/// <summary>
+	/// 原価
+	/// </summary>
+	public float Cost { get; private set; }
+	/// <summary>
+	/// 商品ランク
+	/// </summary>
+	public int Rank { get; private set; }
+	/// <summary>
+	/// 1つ当たりの売却にかかる時間。
+	/// </summary>
+	public float SecondsToSell { get; private set; }
+
+	/// <summary>
+	/// 商品データのコンストラクタ
+	/// </summary>
+	/// <param name="name_">名前</param>
+	/// <param name="unitPrice_">単価</param>
+	/// <param name="cost">原価</param>
+	/// <param name="rank">商品ランク</param>
+	/// <param name="secondsToSelling">1つ当たり売却にかかる時間</param>
+	public ProductData(string name_, 
+		               float unitPrice_, 
+					   float cost_, 
+					   int rank_, 
+					   float secondsToSell_)
+	{
+		Name = name_;
+		UnitPrice = unitPrice_;
+		Cost = cost_;
+		Rank = rank_;
+		SecondsToSell = secondsToSell_;
+	}
+}
