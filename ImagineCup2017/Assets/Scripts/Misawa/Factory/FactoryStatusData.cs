a211b30f382e39f23cd56ae0eb4772dfbbbb0cb4
hmrskw <hmrskw@gmail.com>
2017-01-27 14:27:25 +0900

    マップ生成、工場のモデルクラス、工場のコントローラークラスの実装

---
+++ ImagineCup2017/Assets/Scripts/Misawa/Factory/FactoryStatusData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class FactoryStatusData
+{
+    [Range(0, 3), Tooltip("生産する商品")]
+    public int productType;
+    [Range(1, 100), Tooltip("ランクアップコスト")]
+    public int rankUpcost;
+    [Range(1, 100), Tooltip("生産数")]
+    public int productCount;
+    [Range(1, 100), Tooltip("汚染度")]
+    public float pollutionDegree;
+}
