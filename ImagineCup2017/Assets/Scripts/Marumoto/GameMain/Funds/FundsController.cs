0a0a85e49aafa422d47bdd357ade24aaf0755b7c
Keita Marumoto <marumoto.keita@gmail.com>
2017-02-02 05:40:31 +0900

    ProductRegister.csを更新。FundsController.csを新規作成。および2スクリプトを空のゲームオブジェクトにアタッチ。
    ProductRegister.csを機能ごとに関数分け。同時に商品データのget関数を追加。
    FundsController.csを作成。細かい機能ごとに関数分けしてあり、PublicになっているFundsValueChange(float)から資金額を加減算できる。

---
+++ ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Funds/FundsController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FundsController : MonoBehaviour {
+	[SerializeField]
+	Text fundsText;
+
+	float fundsValue = 10000.0f;
+	string strValue = "";
+
+	void Start()
+	{
+		//FundsValueChange(0.0f);
+	}
+
+	/// <summary>
+	/// 資金額を変更する。
+	/// </summary>
+	/// <param name="diff_">加算なら(+)、減算なら(-)</param>
+	public void FundsValueChange(float diff_)
+	{
+		ValueUpdate(diff_);
+		ValueClamping();
+		DividingValue();
+		TextUpdate();
+	}
+
+	/// <summary>
+	/// 資金額を引数の数だけ加減算する。
+	/// </summary>
+	/// <param name="diff_">加算したいなら（＋）、減算したいなら（ー）</param>
+	void ValueUpdate(float diff_)
+	{
+		fundsValue += diff_;
+		Debug.Log("変更後資金額：" + fundsValue);
+	}
+
+	void ValueClamping()
+	{
+		if (fundsValue < 0)
+		{
+			fundsValue = 0.0f;
+		}
+	}
+
+	/// <summary>
+	/// 3桁ごとにカンマを入れる。
+	/// </summary>
+	void DividingValue()
+	{
+		int intValue_ = (int)fundsValue;
+		string strValue_ = string.Format("{0:#,0}", intValue_);
+		strValue = strValue_;
+	}
+
+	void TextUpdate()
+	{
+		fundsText.text = strValue;
+	}
+}
--- ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Product/ProductRegister.cs
+++ ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Product/ProductRegister.cs
@@ -15,8 +15,16 @@ public class ProductRegister : MonoBehaviour {
 
 	Dictionary<string, ProductData> products = new Dictionary<string, ProductData>();
 
-	// Use this for initialization
 	void Start ()
+	{
+		Register();
+		OutProductsData();
+	}
+
+	/// <summary>
+	/// 読み込んだ商品データをDictionaryでコンテナとして登録
+	/// </summary>
+	void Register()
 	{
 		for (int productDataNum = 0; productDataNum < name_.Count; productDataNum++)
 		{
@@ -27,12 +35,22 @@ public class ProductRegister : MonoBehaviour {
 												   secondsToSell_[productDataNum]);
 			products.Add(product_.Name, product_);
 		}
+	}
 
+	/// <summary>
+	/// 商品データ取得(Dictionary<string,ProductData>)
+	/// </summary>
+	/// <returns>商品データを返却。</returns>
+	public Dictionary<string, ProductData> getProductDatas() { return products; }
+
+	//デバッグ用:商品データを確実に登録できているかの確認用。
+	void OutProductsData()
+	{
 		foreach (KeyValuePair<string, ProductData> prod_ in products)
 		{
-			string message = "Name : " + prod_.Value.Name + " | " 
-				           + "UnitPrice : " + prod_.Value.UnitPrice.ToString() + " | " 
-				           + "Cost : " + prod_.Value.Cost.ToString() + " | "
+			string message = "Name : " + prod_.Value.Name + " | "
+						   + "UnitPrice : " + prod_.Value.UnitPrice.ToString() + " | "
+						   + "Cost : " + prod_.Value.Cost.ToString() + " | "
 						   + "Rank" + prod_.Value.Rank.ToString() + " | "
 						   + "Seconds : " + prod_.Value.SecondsToSell.ToString() + " | "
 						   + "Number : " + prod_.Value.NumberOfProducts.ToString();
