e6e06db28317c5854d24b49f65acbd661122c0ac
Keita Marumoto <marumoto.keita@gmail.com>
2017-02-11 12:39:58 +0900

    Tagの追加。Pollution/PollutionModel.csを作成

---
+++ ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Pollution/PollutionModel.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PollutionModel {
+	public float SumPollution { get; private set; }
+	public Dictionary<string, float> Pollutions { get; private set; }
+}
