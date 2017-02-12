606a04aa7297343a32f1bd2840c30e6ad20b8add
hmrskw <hmrskw@gmail.com>
2017-02-12 23:06:10 +0900

    testクラスの名前をFactoryControllerに変更

--- ImagineCup2017/Assets/Scripts/Misawa/Factory/FactoryController.cs
+++ ImagineCup2017/Assets/Scripts/Misawa/Factory/FactoryController.cs
@@ -3,10 +3,10 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class test : MonoBehaviour {
+public class FactoryController : MonoBehaviour {
     enum State
     {
-        MAKE,BUILD,RANKUP
+        MAKE, BUILD, RANKUP
     }
 
     [SerializeField]
@@ -21,61 +21,41 @@ public class test : MonoBehaviour {
     [SerializeField]
     ProductRegister productRegister;
 
-    //int[] productCount = new int[4];
-
     State state;
-    //int buildFactoryID;
-    
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         state = State.MAKE;
-        //buildFactoryID = 0;
-        //foreach (var product in productCount) product.Equals(0);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            if(state == State.MAKE)
+            if (state == State.MAKE)
             {
                 //工場で商品を生産
-                Dictionary<string, int> pro = factoryManager.Make();
+                Dictionary<string, int> productCount = factoryManager.Make();
 
-                foreach (KeyValuePair<string, int> tes in pro)
+                foreach (KeyValuePair<string, int> product in productCount)
                 {
-                    productRegister.NumberOfProductsValueChange(tes.Key,tes.Value);
+                    productRegister.NumberOfProductsValueChange(product.Key, product.Value);
                 }
-                /*for (int i = 0; i < 4; i++)
-                {
-                    productCount[i] += pro[i];
-                }*/
-
-                string log = "";
-                log += "ランク1：" + factoryManager.GetFactoriesCount(0, 1).ToString() + "\n";
-                log += "ランク2：" + factoryManager.GetFactoriesCount(0, 2).ToString() + "\n";
-                log += "ランク3：" + factoryManager.GetFactoriesCount(0, 3).ToString() + "\n";
-
-                log += "ランク1：" + factoryManager.GetFactoriesCount(1, 1).ToString() + "\n";
-                log += "ランク2：" + factoryManager.GetFactoriesCount(1, 2).ToString() + "\n";
-                log += "ランク3：" + factoryManager.GetFactoriesCount(1, 3).ToString() + "\n";
-
-                Debug.Log(log);
-                productRegister.tesLog();
             }
         }
     }
 
     public void OnClickBuildButton(int factoryID)
     {
-        //buildFactoryID = factoryID;
         state = State.BUILD;
         StartCoroutine(BuildNewFactory(factoryID));
     }
 
     IEnumerator BuildNewFactory(int buildFactoryID)
     {
-        while(state == State.BUILD)
+        while (state == State.BUILD)
         {
             if (Input.GetMouseButtonDown(0))
             {
