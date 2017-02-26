741a0d00c8ccdf4a4f30acfb8dd89fa698022f49
Keita Marumoto <marumoto.keita@gmail.com>
2017-02-26 09:03:23 +0900

    ガチャ要素の追加。まだ不完全。

---
+++ ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Gacha/GachaButtonYes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GachaButtonYes : MonoBehaviour {
+	[SerializeField]
+	GameObject gachaPopup;
+
+	public void CreatePopup()
+	{
+		Instantiate(gachaPopup, GachaManager.Instance.getParent(), false);
+	}
+}
---
+++ ImagineCup2017/Assets/Scripts/Marumoto/GameMain/Gacha/GachaManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GachaManager : MonoBehaviour {
+	private static GachaManager instance;
+	public static GachaManager Instance
+	{
+		get { return instance; }
+	}
+
+	void Awake()
+	{
+		if (instance == null) instance = this;
+	}
+
+	public Transform getParent()
+	{
+		return transform;
+	}
+}
